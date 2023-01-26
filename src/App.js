92fdbb274dd46b18dab2766bb8bdbcecf2338465
Luciane Dias <lucilewa@yahoo.com>
2023-01-26 10:04:55 -0800

    installed bootstrap, created env file, connected env file to giphy api website, index.html with link, app.js with code and app.css with  background

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Card, Container, Row, Col } from "react-bootstrap";
 import "./App.css";
-import React from "react";
+import axios from "axios";
 
-function BasicExample() {
+const App = () => {
+  const [giphy, setGiphy] = useState("");
+  const [fetching, setFetching] = useState("false");
+  useEffect(() => {
+    const fetchData = async () => {
+      const apiRoot = "https://api.giphy.com/v1/gifs/";
+      const api_key = process.env.REACT_APP_GIPHY_KEY;
+      const result = await axios(`${apiRoot}trending?api_key=${api_key}`);
+      console.log(result);
+      const randomIndex = Math.floor(Math.random() * 50);
+      setGiphy(`${result.data.data[randomIndex].images.fixed_height.url}`);
+    };
+    fetchData();
+  }, [fetching]);
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Meme</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Next</Button>
-      </Card.Body>
-    </Card>
+    <div>
+      <Container style={{ marginTop: "100px" }}>
+        <Row>
+          <Col md={{ span: 6, offset: 3 }}>
+            <Card style={{ backgroundColor: "powderblue" }}>
+              <Card.Img
+                variant="top"
+                src={giphy}
+                style={{ height: "350px", width: "100%" }}
+              />
+              <Card.Body>
+                <Card.Title>What do you Meme?</Card.Title>
+                <Card.Text>
+                  Pick a card with a caption that match this Meme
+                </Card.Text>
+                <Button
+                  variant="primary"
+                  onClick={() => setFetching(!fetching)}
+                >
+                  click here to see your next Meme
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
-}
+};
 
-export default BasicExample;
+export default App;
