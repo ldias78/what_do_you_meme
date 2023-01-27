493b441b1f023a5216c58bf3dd3241446bb288e1
Diana Soriano <dssori@gmail.com>
2023-01-27 12:38:21 -0500

    updated this branch

--- src/App.js
+++ src/App.js
@@ -6,18 +6,74 @@ import "bootstrap";
 import { Button, Form, InputGroup } from "react-bootstrap";
 
 function App() {
-  return (
-    <div className="welcome-screen">
-      <InputGroup className="player-name">
-        <InputGroup.Text> What's your name? </InputGroup.Text>
-        <Form.Control aria-label="Name" />
-      </InputGroup>
 
-      <Button> Test Button </Button>
-      <Button> Test Button 2 </Button>
+  import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Card, Container, Row, Col } from "react-bootstrap";
+import "./App.css";
+import axios from "axios";
+
+const App = () => {
+  const [giphy, setGiphy] = useState("");
+  const [fetching, setFetching] = useState("false");
+  const [prevgiphy, setCurrentGiph] = useState("");
+  useEffect(() => {
+    const fetchData = async () => {
+      const apiRoot = "https://api.giphy.com/v1/gifs/";
+      const api_key = process.env.REACT_APP_GIPHY_KEY;
+      const result = await axios(
+        `${apiRoot}trending?api_key=${api_key}&rating=g`
+      );
+      console.log(result);
+      const randomIndex = Math.floor(Math.random() * 50);
+      setGiphy(`${result.data.data[randomIndex].images.fixed_height.url}`);
+    };
+    fetchData();
+  }, [fetching]);
+  return (
+    <div>
+      <Container style={{ marginTop: "100px" }}>
+        <Row>
+          <Col md={{ span: 6, offset: 3 }}>
+            <Card style={{ backgroundColor: "powderblue" }}>
+              <Card.Img
+                variant="top"
+                src={giphy}
+                style={{ height: "350px", width: "100%" }}
+              />
+              <Card.Body>
+                <Card.Title>What do you Meme?</Card.Title>
+                <Card.Text>
+                  Pick a card with a caption that match this Meme
+                </Card.Text>
+                <Button
+                  variant="primary"
+                  onClick={() => setFetching(!fetching)}
+                >
+                  click here to see your next Meme
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
-}
+};
+
+export default App;
+
+  // return (
+  //   <div className="welcome-screen">
+  //     <InputGroup className="player-name">
+  //       <InputGroup.Text> What's your name? </InputGroup.Text>
+  //       <Form.Control aria-label="Name" />
+  //     </InputGroup>
+
+  //     <Button> Test Button </Button>
+  //     <Button> Test Button 2 </Button>
+  //   </div>
+
 
 // function BasicExample() {
 //   return (
