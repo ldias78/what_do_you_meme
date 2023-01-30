830121630bd3db13c6ab38982b7c4ae75604b174
Diana Soriano <dssori@gmail.com>
2023-01-30 11:51:19 -0500

    updated card meme deck with useState

--- src/CardsMemeDeck.js
+++ src/CardsMemeDeck.js
@@ -2,23 +2,18 @@ import React from "react";
 import Card from "src/Card.js";
 
 const CardDeck = () => {
-  const [totaldeck, setTotaldeck] = (React.useState = React.useState(25));
+  const [totaldeck, setTotaldeck] = React.useState(25);
 
   const shuffledeck = () => {
-    setTotaldeck((prevTotal) => prevTotal - 7);
+    setTotaldeck((prevTotal) => totaldeck - 7);
     console.log("hi from deck");
-    this.setState((prevState) => {
-      return {
-        Totaldeck: prevState.Totaldeck * 7,
-      };
-    });
   };
 
   return (
     <div className="carddeck">
       <Card />
       <span className="deck">totaldeck</span>
-      <button className="shuffle" onClick={this.shuffledeck}>
+      <button className="shuffle" onClick={() => shuffledeck()}>
         Shuffle Button
       </button>
     </div>
