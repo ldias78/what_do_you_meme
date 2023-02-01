7c727f4d2347df45d7e33586b225f1f9f7037033
Diana Soriano <dssori@gmail.com>
2023-02-01 12:08:20 -0500

    Player Board update

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -4,16 +4,37 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // from {Player}"./MainPage";
 
 const Player = () => {
-  <div>
-    <p class="h3">{PlayerName}</p>
-  </div>;
-  <div>
-    <p class="h3">{PlayerName}</p>
-  </div>;
-  <div>
-    <p class="h3">{PlayerName}</p>
-  </div>;
-  <div>
-    <p class="h3">{PlayerName}</p>
-  </div>;
+  const PlayerTop = () => {
+    return (
+      <div style={{ position: "absolute", top: 0, left: 0 }}>
+        {/* {Player.name} */}
+      </div>
+    );
+  };
+
+  const PlayerRight = () => {
+    return (
+      <div style={{ position: "absolute", top: 0, right: 0 }}>
+        {/* {Player.name} */}
+      </div>
+    );
+  };
+
+  const BottomLeft = () => {
+    return (
+      <div style={{ position: "absolute", bottom: 0, left: 0 }}>
+        {" "}
+        {/* {Player.name} */}
+      </div>
+    );
+  };
+
+  const BottomRight = () => {
+    return (
+      <div style={{ position: "absolute", bottom: 0, right: 0 }}>
+        {" "}
+        {/* {Player.name} */}
+      </div>
+    );
+  };
 };
