fa6d8b0722a846be3e67bbf0215983764437274f
Luciane Dias <lucilewa@yahoo.com>
2023-01-31 18:36:22 -0800

    Created main page and board game page, main page redirect to board game

---
+++ src/BoardGame.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Card, Container, Row, Col } from "react-bootstrap";
+import "./BoardGame.css";
+import axios from "axios";
+
+const BoardGame = () => {
+  const [giphy, setGiphy] = useState("");
+  const [fetching, setFetching] = useState("false");
+  useEffect(() => {
+    const fetchData = async () => {
+      const apiRoot = "https://api.giphy.com/v1/gifs/";
+      const api_key = process.env.REACT_APP_GIPHY_KEY;
+      const result = await axios(`${apiRoot}trending?api_key=${api_key}`);
+      console.log(result);
+      const randomIndex = Math.floor(Math.random() * 50);
+      setGiphy(`${result.data.data[randomIndex].images.fixed_height.url}`);
+    };
+    fetchData();
+  }, [fetching]);
+  return (
+    <div>
+      <Container style={{ marginTop: "100px" }}>
+        <Row>
+          <Col md={{ span: 6, offset: 3 }}>
+            <Card style={{ backgroundColor: "powderblue" }}>
+              <Card.Img
+                variant="top"
+                src={giphy}
+                style={{ height: "350px", width: "100%" }}
+              />
+              <Card.Body>
+                <Card.Title>What do you Meme?</Card.Title>
+                <Card.Text>
+                  Pick a card with a caption that matches this Meme
+                </Card.Text>
+                <Button
+                  variant="primary"
+                  onClick={() => setFetching(!fetching)}
+                >
+                  click here to see your next Meme
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default BoardGame;
