862962697f80428892440aa002196d5153c32e6f
Diana Soriano <dssori@gmail.com>
2023-02-02 13:23:51 -0500

    updated CardDeck to account for 100 cards

--- src/Components/CardDeck.js
+++ src/Components/CardDeck.js
@@ -1,19 +1,12 @@
 import React, { useState } from "react";
 import Card from "./Card";
-import CardCaptions from "./CardsCaptions";
 
 const CardDeck = () => {
-  const [deck, setDeck] = useState(CardCaptions);
+  const deckCount = 100;
+  const [deck, setDeck] = useState(
+    Array.from({ length: deckCount }, (_, i) => i + 1)
+  );
   const [players, setPlayers] = useState([]);
-
-  const shuffleDeck = () => {
-    setDeck(
-      deck
-        .map((a) => [Math.random(), a])
-        .sort((a, b) => a[0] - b[0])
-        .map((a) => a[1])
-    );
-  };
 
   const dealCards = () => {
     if (deck.length === 0) {
@@ -22,7 +15,9 @@ const CardDeck = () => {
 
     const newPlayer = [];
     for (let i = 0; i < 7; i++) {
-      newPlayer.push(deck.pop());
+      const randomIndex = Math.floor(Math.random() * deck.length);
+      const card = deck.splice(randomIndex, 1)[0];
+      newPlayer.push(card);
     }
 
     setPlayers([...players, newPlayer]);
@@ -30,23 +25,15 @@ const CardDeck = () => {
 
   return (
     <div className="carddeck">
-      <div className="deck">
-        {deck.map((card, index) => (
-          <Card key={index} caption={card} />
-        ))}
-      </div>
       <div className="players">
         {players.map((hand, index) => (
           <div key={index} className="player-hand">
             {hand.map((card, index) => (
-              <Card key={index} caption={card} />
+              <Card key={index} number={card} />
             ))}
           </div>
         ))}
       </div>
-      <button className="shuffle" onClick={shuffleDeck}>
-        Shuffle Deck
-      </button>
       <button className="deal" onClick={dealCards}>
         Deal 7 Cards
       </button>
--- src/Components/PlayerBoard.js
+++ src/Components/PlayerBoard.js
@@ -4,8 +4,15 @@ import CardCaptions from "./CardsCaptions";
 import Player from "./Player";
 
 const PlayerBoard = () => {
+  // get player inside player board
+  //get carddeck inside player board
+  //get code to take turns by player
+  //lets vote button
   <div>
     <Player></Player>
     <CardDeck></CardDeck>
+    <div>MEME</div>
+    <div>LETS VOTE</div>
+    <div>TIMER</div>
   </div>;
 };
