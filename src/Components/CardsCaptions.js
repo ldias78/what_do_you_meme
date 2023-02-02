54ca89b47d79d070e5f8bc1c508a88c09ec68067
Diana Soriano <dssori@gmail.com>
2023-02-02 14:21:10 -0500

    commit before switch

---
+++ src/Components/CardsCaptions.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import Randomphrases from ".RandomPhrases";
+
+const CardCaptions = (props) => {
+  return (
+    <div className="cardcap">
+      <p>{this.props.Randomphrases}</p>;
+    </div>
+  );
+};
+
+export default CardCaptions;
--- src/Components/PlayerBoard.js
+++ src/Components/PlayerBoard.js
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
-import { navigate } from "@reach/router";
-import CardCaptions from "./CardsCaptions";
-import Player from "./Player";
+// import React, { useState } from "react";
+// import { navigate } from "@reach/router";
+// import CardCaptions from "./CardsCaptions";
+// import Player from "./Player";
 
-const PlayerBoard = () => {
-  <div>
-    <Player></Player>
-    <CardDeck></CardDeck>
-    <CardCaptions></CardCaptions>
-    <Timer></Timer>
-  </div>;
-};
+// const PlayerBoard = () => {
+//   <div>
+//     <Player></Player>
+//     <CardDeck></CardDeck>
+//     <CardCaptions></CardCaptions>
+//     <Timer></Timer>
+//   </div>;
+// };
