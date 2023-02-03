9277e89197d819bc9f4c898c86dc192aadd14455
Diana Soriano <dssori@gmail.com>
2023-02-02 22:38:11 -0500

    editing dealhand

--- src/Components/DealHand.js
+++ src/Components/DealHand.js
@@ -1,19 +1,52 @@
-import React, { useEffect, useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import MainPage from "./MainPage";
+import React from "react";
 
-const HandleCard = ({ frontCard, backCard }) => {
-  const [isFlipped, setIsFlipped] = useState(false);
-
+const DealHand = ({ content, answer, id }) => {
   return (
-    <div style={styles.cardContainer} onClick={() => setIsFlipped(!isFlipped)}>
-      <img
-        src={isFlipped ? backCard : frontCard}
-        alt="card"
-        style={styles.cardImage}
-      />
+    <div
+      className={`card ${answer ? "card--answer" : ""} ${
+        id ? `card--${id}` : ""
+      }`}
+    >
+      <div className="card__content">{content}</div>
+      <footer className="card__footer">
+        {answer ? (
+          <img src="http://blog.codepen.io/wp-content/uploads/2012/06/Button-Black-Small.png" />
+        ) : (
+          <img src="http://blog.codepen.io/wp-content/uploads/2012/06/Button-White-Small.png" />
+        )}
+        <span>Cards Against Developers</span>
+      </footer>
     </div>
   );
 };
 
-export default HandleCard;
+// const HandleRadio = () => {
+//   const [checked, setChecked] = React.useState(true);
+
+//   return (
+//     <div>
+//       <input type="radio" name="choice" id="spaces" checked={checked} onChange={() => setChecked(true)} />
+//       <input type="radio" name="choice" id="tabs" checked={!checked} onChange={() => setChecked(false)} />
+//       <label htmlFor="tabs">Tabs</label>
+//       <label htmlFor="spaces">Spaces</label>
+//       <Card content="Tabs" answer id="tabs" />
+//       <Card content="Spaces" answer id="spaces" />
+//     </div>
+//   );
+// };
+
+export default DealHand;
+
+//   const [isFlipped, setIsFlipped] = useState(false);
+
+//     <div style={styles.cardContainer} onClick={() => setIsFlipped(!isFlipped)}>
+//       <img
+//         src={isFlipped ? backCard : frontCard}
+//         alt="card"
+//         style={styles.cardImage}
+//       />
+//     </div>
+//   );
+// };
+
+// export default HandleCard;
