ef1ff28c12e5adf17a050475c31fe11cfde13e2c
Diana Soriano <dssori@gmail.com>
2023-02-04 21:26:11 -0500

    captions are working

---
+++ src/Components/CardCaptions.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Randomphrases from "./Randomphrases.json";
+import "bootstrap/dist/css/bootstrap.css";
+
+const CardCaptions = (props) => {
+  const randomCaptions = Math.floor(
+    Math.random() * Randomphrases.Randomphrases.length
+  );
+  const caption = Randomphrases.Randomphrases[randomCaptions];
+
+  return (
+    <div className="card text-dark bg-light mb-3">
+      <div className="card-body">
+        <h5 className="card-title">{caption.quote}</h5>
+      </div>
+    </div>
+  );
+};
+
+export default CardCaptions;
