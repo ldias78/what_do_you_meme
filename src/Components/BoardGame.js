e13acf9b214d8ae86274a859e64aeefb0e348500
Luciane Dias <lucilewa@yahoo.com>
2023-02-04 23:42:54 -0800

    changed background in the main page, added bottoms to each player to redirect to the player cards

--- src/Components/BoardGame.js
+++ src/Components/BoardGame.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button, Card, Container, Row, Col } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import "./BoardGame.css";
 import axios from "axios";
 import { navigate } from "@reach/router";
@@ -9,6 +9,7 @@ const BoardGame = () => {
   const [giphy, setGiphy] = useState("");
   const [fetching, setFetching] = useState(false);
   const [selectedMeme, setSelectedMeme] = useState("");
+  // eslint-disable-next-line
   const [players, setPlayers] = useState([
     { name: "Player 1", score: 0 },
     { name: "Player 2", score: 0 },
@@ -34,51 +35,102 @@ const BoardGame = () => {
   const handleVote = () => {
     navigate("/voteboard", { state: { selectedMeme } });
   };
+
   return (
     <div className="boardgame-container">
-      <div className="player-1">
-        <h2>Player 1</h2>
+      <h1 className="title">
+        WELCOME TO THE WHAT DO YOU MEME GAME
+        <p> GIPHY VERSION</p>
+      </h1>
+      <header>
+        <div className="header-container">
+          <div className="player-container">
+            <h2>Player 1</h2>
+            <Button
+              variant="secondary"
+              className="big-btn see-card-btn"
+              onClick={() =>
+                navigate("/player1cards", { state: { player: players[0] } })
+              }
+            >
+              Get your Cards
+            </Button>
+          </div>
+          <div className="player-container">
+            <h2>Player 2</h2>
+            <Button
+              variant="secondary"
+              className="big-btn see-card-btn"
+              onClick={() =>
+                navigate("/player2cards", { state: { player: players[1] } })
+              }
+            >
+              Get your Cards
+            </Button>
+          </div>
+        </div>
+      </header>
+      <footer>
+        <div className="footer-container">
+          <div className="player-container">
+            <h2>Player 3</h2>
+            <Button
+              variant="secondary"
+              className="big-btn see-card-btn"
+              onClick={() =>
+                navigate("/player3cards", { state: { player: players[2] } })
+              }
+            >
+              Get your Cards
+            </Button>
+          </div>
+          <div className="player-container">
+            <h2>Player 4</h2>
+            <Button
+              variant="secondary"
+              className="big-btn see-card-btn"
+              onClick={() =>
+                navigate("/player4cards", { state: { player: players[3] } })
+              }
+            >
+              Get your Cards
+            </Button>
+          </div>
+        </div>
+      </footer>
+      <div className="meme-container">
+        <div style={{ textAlign: "center", marginTop: "120px" }}>
+          <Card style={{ backgroundColor: "powderblue" }}>
+            <Card.Img
+              variant="top"
+              src={giphy}
+              style={{ height: "350px", width: "100%" }}
+              onClick={() => handleMemeSelection(giphy)}
+            />
+            <Card.Body>
+              <Card.Title style={{ textAlign: "center" }}>
+                Look this Giphy and match your caption card
+              </Card.Title>
+              <Card.Text style={{ textAlign: "center" }}>
+                What caption makes you laugh when you look at this Meme?
+              </Card.Text>
+              <Button
+                variant="primary"
+                style={{ display: "block", margin: "0 auto" }}
+                onClick={() => setFetching(!fetching)}
+              >
+                Next Meme
+              </Button>
+            </Card.Body>
+          </Card>
+        </div>
       </div>
-      <div className="player-2">
-        <h2>Player 2</h2>
-      </div>
-      <Container style={{ marginTop: "160px" }}>
-        <Row>
-          <Col md={{ span: 4, offset: 4 }}>
-            <Card style={{ backgroundColor: "powderblue" }}>
-              <Card.Img
-                variant="top"
-                src={giphy}
-                style={{ height: "350px", width: "100%" }}
-                onClick={() => handleMemeSelection(giphy)}
-              />
-              <Card.Body>
-                <Card.Title style={{ textAlign: "center" }}>
-                  Look this Giphy and match your caption card
-                </Card.Title>
-                <Card.Text style={{ textAlign: "center" }}>
-                  What caption makes you laugh when you look at this Meme?
-                </Card.Text>
-                <Button
-                  variant="primary"
-                  style={{ display: "block", margin: "0 auto" }}
-                  onClick={() => setFetching(!fetching)}
-                >
-                  Next Meme
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-      <div className="player-3">
-        <h2>Player 3</h2>
-      </div>
-      <div className="player-4">
-        <h2>Player 4</h2>
-      </div>
-      <div style={{ textAlign: "center", marginTop: "20px" }}>
-        <Button variant="secondary" onClick={handleVote}>
+      <div style={{ textAlign: "center", marginTop: "120px" }}>
+        <Button
+          variant="secondary"
+          onClick={handleVote}
+          style={{ fontSize: "3em" }}
+        >
           Let's Vote
         </Button>
       </div>
--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -7,20 +7,9 @@ const MainPage = () => {
   const [playerName, setPlayerName] = useState("");
   const [inputDisabled, setInputDisabled] = useState(false);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (players.length === 4) {
-      navigate("/boardgame", { state: { players } });
-    }
-  };
-
-  const handleChange = (e) => {
-    setPlayerName(e.target.value);
-  };
-
   const handleDone = () => {
     if (players.length < 4 && playerName !== "") {
-      setPlayers([...players, playerName]);
+      setPlayers([...players, `Player ${players.length + 1}: ${playerName}`]);
       setPlayerName("");
     }
     if (players.length === 3) {
@@ -28,19 +17,28 @@ const MainPage = () => {
     }
   };
 
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+    }
+  };
+
   return (
     <div className="main-page">
+      <video src="/videos/video-2.mp4" autoPlay loop muted />
       <h1 className="title">WELCOME TO THE WHAT DO YOU MEME GAME</h1>
-      <form onSubmit={handleSubmit}>
+
+      <form>
         <div>
           <label>
-            Player Name:
+            <h2>Player Name:</h2>
             <input
               type="text"
               value={playerName}
-              onChange={handleChange}
+              onChange={(e) => setPlayerName(e.target.value)}
               required
               disabled={inputDisabled}
+              onKeyPress={handleKeyPress}
             />
           </label>
           <button type="button" onClick={handleDone} disabled={inputDisabled}>
@@ -49,13 +47,15 @@ const MainPage = () => {
         </div>
         <div className="player-list">
           {players.map((player, i) => (
-            <div key={i}>
-              Player {i + 1}: {player}
-            </div>
+            <div key={i}>{player}</div>
           ))}
         </div>
         {players.length === 4 && (
-          <button type="submit" className="start-game-button">
+          <button
+            type="button"
+            className="start-game-button"
+            onClick={() => navigate("/boardgame", { state: { players } })}
+          >
             Start Game
           </button>
         )}
