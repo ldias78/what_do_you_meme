8ed0ff1a42d52e61b34bbe3882a9ff9ed5853c9d
Diana Soriano <dssori@gmail.com>
2023-02-06 22:16:37 -0500

    carddeck js on master

---
+++ src/Components/CardDeck.stories.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import CardDeck from "./GameLogic";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Example/CardDeck",
+  component: CardDeck,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <CardDeck {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  primary: true,
+  label: "CardDeck",
+};
