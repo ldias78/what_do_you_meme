b6e781c168bea8cd046d88aeca7da50210aa6aa4
Diana Soriano <dssori@gmail.com>
2023-02-07 15:23:07 -0500

    shuffle deck function updated

--- src/Components/ShuffleDeck.js
+++ src/Components/ShuffleDeck.js
@@ -2,19 +2,21 @@ import React, { useState } from "react";
 import CardDeck from "./CardDeck";
 
 const ShuffleDeck = () => {
-  const [deck, setDeck] = useState(CardDeck);
+  const [shuffleDeck, setshuffleDeck] = useState(CardDeck);
 
-  setDeck(
-    deck
+  setshuffleDeck(
+    shuffleDeck
       .map((a) => [Math.random(), a])
       .sort((a, b) => a[0] - b[0])
       .map((a) => a[1])
   );
+  console.log(setshuffleDeck);
+
   return (
     <div className="cards">
       <div className="card"></div>
       <p>
-        <CardDeck />
+        <CardDeck deck={shuffleDeck} />
       </p>
     </div>
   );
