bdefa56afc159ebc0de689ce83c5df4efd56acc2
Diana Soriano <dssori@gmail.com>
2023-02-09 07:59:32 -0500

    selected card starting code

---
+++ src/Components/SelectedCard.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import Randomphrases from "./Randomphrases.json";
+import PlayerHand from "./PlayerHand.js";
+import "./CardDeck.css";
+
+const SelectedCard = () => {};
+    
+    return (
+    <div>
+    <h2>Player 1</h2>
+    {players[0].map((quote, index) => (
+      <Card key={index} quote={quote} handleCardClick={handleCardClick} />
+    ))}
+    <h2>Player 2</h2>
+    {players[1].map((quote, index) => (
+      <Card key={index} quote={quote} handleCardClick={handleCardClick} />
+    ))}
+    <h2>Player 3</h2>
+    {players[2].map((quote, index) => (
+      <Card key={index} quote={quote} handleCardClick={handleCardClick} />
+    ))}
+    <h2>Player 4</h2>
+    {players[3].map((quote, index) => (
+      <Card key={index} quote={quote} handleCardClick={handleCardClick} />
+    ))}
+    {selectedCard ? <p>Selected card: {selectedCard.quote}</p> : null}
+  </div>
+    )};
+
+    
