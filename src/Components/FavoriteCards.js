b92a8f1ee5bc5919bef470a7985472a303e75531
Diana Soriano <dssori@gmail.com>
2023-02-10 12:37:56 -0500

    updatedfiles

---
+++ src/Components/FavoriteCards.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+
+const FavoriteCards = () => {
+  useEffect(() => {}, []);
+
+  return (
+    <div>
+      <div>All Favorite cards</div>
+    </div>
+  );
+};
+
+export default FavoriteCards;
