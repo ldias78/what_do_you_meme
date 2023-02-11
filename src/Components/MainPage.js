0756a519911f2c6247f877383be5ef3bebae87fc
Diana Soriano <dssori@gmail.com>
2023-02-10 19:16:01 -0500

    updated mainpage

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -7,16 +7,12 @@ const MainPage = () => {
   const [playerName, setPlayerName] = useState("");
   const [inputDisabled, setInputDisabled] = useState(false);
 
-  const handleClick = () => {
-    navigate("/boardgame", {
-      state: { players: players },
-    });
+  const handleStartGame = (names) => {
+    navigate("/boardgame", { state: { names } });
   };
-
   const handleDone = () => {
     if (players.length < 4 && playerName !== "") {
-      const playerInfo = { playerName, score: 0 };
-      setPlayers([...players, playerInfo]);
+      setPlayers([...players, `Player ${players.length + 1}: ${playerName}`]);
       setPlayerName("");
     }
     if (players.length === 3) {
@@ -33,11 +29,12 @@ const MainPage = () => {
   return (
     <div className="main-page">
       <video src="/videos/video-2.mp4" autoPlay loop muted />
+      <h1 className="title">WELCOME TO THE WHAT DO YOU MEME GAME</h1>
 
       <form className="input-container">
         <div>
           <label>
-            <h1>Player Name:</h1>
+            <h2>Player Name:</h2>
             <input
               type="text"
               value={playerName}
@@ -55,7 +52,7 @@ const MainPage = () => {
       <div className="player-list-container">
         <div className="player-list">
           {players.map((player, i) => (
-            <div key={i}>{player.playerName}</div>
+            <div key={i}>{player}</div>
           ))}
         </div>
       </div>
@@ -63,7 +60,10 @@ const MainPage = () => {
         <button
           type="button"
           className="start-game-button"
-          onClick={handleClick}
+          onClick={() => {
+            localStorage.removeItem("FavoriteCards");
+            navigate("/boardgame", { state: { players } });
+          }}
         >
           Start Game
         </button>
