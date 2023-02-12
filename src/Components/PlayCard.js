186aa8f3fb551caf2b5a651cd11cf6744faf69dc
Diana Soriano <dssori@gmail.com>
2023-02-12 10:06:57 -0500

    mainpage updated

--- src/Components/PlayCard.js
+++ src/Components/PlayCard.js
@@ -3,28 +3,24 @@ import Randomphrases from "./Randomphrases.json";
 import "./CardDeck.css";
 import { navigate } from "@reach/router";
 import Card from "./Card";
-
+import { NUMBER_OF_CARDS } from "../constant";
 const PlayCard = (props) => {
   const [cards, setCards] = useState([]);
   const [selectedCard, setSelectedCard] = useState(null);
-  console.log("player33", props);
   const player = props.location.state.player;
-  console.log("player44", player);
-  // let players = [[], [], [], []];
   useEffect(() => {
-    let numberOfCards = 7;
     const shuffledArray = Randomphrases.sort(() => 0.5 - Math.random());
-    const chooenItems = shuffledArray.slice(0, numberOfCards);
+    const chooenItems = shuffledArray.slice(0, NUMBER_OF_CARDS);
     setCards(chooenItems);
   }, [player]);
-
+  const isLastPlayer = player.startsWith("Player 4:");
   return (
     <div
       style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
     >
       <h1
         style={{
-          marginBottom: 100,
+          marginBottom: 50,
           marginTop: 100,
           color: "green",
           fontFamily: "Comic Sans MS, sans-serif",
@@ -32,6 +28,33 @@ const PlayCard = (props) => {
       >
         {player}
       </h1>
+      <button
+        style={{
+          marginBottom: 50,
+          backgroundColor: "green",
+          transition: "all 0.5s ease",
+          padding: 15,
+          fontSize: 18,
+          borderRadius: 5,
+        }}
+        onClick={() => {
+          if (isLastPlayer) {
+            navigate("favoritecards");
+          } else {
+            window.history.back();
+          }
+        }}
+        onMouseEnter={(e) => {
+          e.currentTarget.style.backgroundColor = "coral";
+          e.currentTarget.style.transform = "scale(1.2)";
+        }}
+        onMouseLeave={(e) => {
+          e.currentTarget.style.backgroundColor = "green";
+          e.currentTarget.style.transform = "scale(1)";
+        }}
+      >
+        {isLastPlayer ? "Go to All Favorite cards" : "Back to meme"}
+      </button>
       <div style={{ display: "flex", flexWrap: "wrap" }}>
         {cards.map((quote, index) => (
           <Card
@@ -56,7 +79,6 @@ const PlayCard = (props) => {
               <p
                 style={{
                   marginBottom: 50,
-
                   color: "green",
                   fontFamily: "Comic Sans MS, sans-serif",
                 }}
@@ -67,29 +89,7 @@ const PlayCard = (props) => {
           </div>
         )}
       </div>
-      <button
-        style={{
-          marginTop: 20,
-          backgroundColor: "green",
-          transition: "all 0.5s ease",
-          padding: 15,
-          fontSize: 18,
-          borderRadius: 5,
-        }}
-        onClick={() => navigate("/favoritecards")}
-        onMouseEnter={(e) => {
-          e.currentTarget.style.backgroundColor = "coral";
-          e.currentTarget.style.transform = "scale(1.2)";
-        }}
-        onMouseLeave={(e) => {
-          e.currentTarget.style.backgroundColor = "green";
-          e.currentTarget.style.transform = "scale(1)";
-        }}
-      >
-        Go to All Favorite cards
-      </button>
     </div>
   );
 };
-
 export default PlayCard;
