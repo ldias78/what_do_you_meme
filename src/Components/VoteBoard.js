8525be5d362d5b1bc8d69be62b6b1d69340185a2
Diana Soriano <dssori@gmail.com>
2023-02-13 15:34:33 -0500

    voteboard update

--- src/Components/VoteBoard.js
+++ src/Components/VoteBoard.js
@@ -1,87 +1,90 @@
-import React, { useEffect, useState } from "react";
-import { Table } from "react-bootstrap";
+import React from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import { useNavigate } from "@reach/router";
 import "./VoteBoard.css";
 
 const VoteBoard = ({ giphy, location }) => {
   const navigate = useNavigate();
-  const [scores, setScores] = useState([]);
-  useEffect(() => {
-    const savedItemsStr = localStorage.getItem("FavoriteCards") ?? "{}";
-    const savedItems = JSON.parse(savedItemsStr);
-    let players = {};
-    Object.keys(savedItems).forEach((round) => {
-      Object.keys(savedItems[round]).forEach((p) => {
-        const temp = {
-          player: p,
-          totalScore: players[p]
-            ? players[p].totalScore + savedItems[round][p].count
-            : savedItems[round][p].count ?? 0,
-        };
-        players[p] = temp;
-      });
-    });
-    let scores = [];
-    Object.keys(players).forEach((element) => {
-      scores.push({
-        player: players[element].player,
-        totalScore: players[element].totalScore,
-      });
-    });
-    setScores(scores.sort((a, b) => b.totalScore - a.totalScore));
-  }, []);
+  const players = location?.state?.players;
 
   const handleClick = () => {
-    navigate("/");
+    navigate("/boardgame", {
+      state: { players: players },
+    });
   };
 
   return (
-    <div className="container">
-      <h1
-        style={{
-          textAlign: "center",
-          marginTop: "-60px",
-          marginBottom: "60px",
-          color: "green",
-        }}
-      >
-        CONGRATULATIONS TO THE WINNER!
-      </h1>
-      <video
-        className="video-background"
-        src="/videos/video-3.mp4"
-        autoPlay
-        loop
-        muted
-      />
-      <div className="table-container">
-        <Table striped bordered hover style={{ marginTop: "100px" }}>
-          <thead style={{ backgroundColor: "coral" }}>
-            <tr>
-              <th>#</th>
-              <th>Player Name</th>
-              <th>Score</th>
-            </tr>
-          </thead>
-          <tbody>
-            {scores.map((player, index) => (
-              <tr
-                key={index}
-                style={{
-                  backgroundColor: index % 2 === 0 ? "green" : "lightgrey",
-                }}
-              >
-                <td>{index + 1}</td>
-                <td>{player.player}</td>
-                <td>{player.totalScore}</td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-        <button className="start-over" onClick={handleClick}>
-          START OVER
-        </button>
-      </div>
+    <div>
+      <h1 className="title">NOW LET'S VOTE FOR THE BEST CAPTION</h1>
+      <h3 style={{ textAlign: "center" }}>
+        PICK THE CAPTION THAT MAKES MORE SENSE WITH THE GIPHY MEME DISPLAYED
+      </h3>
+      <video src="/videos/video-3.mp4" autoPlay loop muted />
+      <header>
+        <div className="header-container">
+          {players.slice(0, 2).map((player, index) => (
+            <div className="player-container" key={index}>
+              <h2>{player.playerName}</h2>
+            </div>
+          ))}
+        </div>
+      </header>
+      <footer>
+        <div className="footer-container">
+          {players.slice(2, 4).map((player, index) => (
+            <div className="player-container" key={index}>
+              <h2>{player.playerName}</h2>
+            </div>
+          ))}
+        </div>
+      </footer>
+      <Container style={{ marginTop: "160px" }}>
+        <Row>
+          <Col md={{ span: 4, offset: 4 }}>
+            <div className="meme-container">
+              <div style={{ textAlign: "center", marginTop: "20px" }}>
+                <div style={{ backgroundColor: "powderblue", padding: "30px" }}>
+                  <img
+                    src={giphy}
+                    alt=""
+                    style={{
+                      height: "350px",
+                      width: "100%",
+                      display: "block",
+                      margin: "0 auto",
+                    }}
+                  />
+                  <div style={{ textAlign: "center", marginTop: "30px" }}>
+                    {/* <h2>Look this Giphy and match your caption card</h2> */}
+                    <p> What caption matches better with this Meme?</p>
+                  </div>
+                  <Button
+                    variant="primary"
+                    size="lg"
+                    block
+                    onClick={handleClick}
+                  >
+                    Next Game
+                  </Button>
+
+                  <Row style={{ marginTop: "30px" }}>
+                    <Col md={{ span: 3.1, offset: 3.1 }}>
+                      <Button
+                        variant="danger"
+                        size="lg"
+                        block
+                        onClick={() => navigate("/winner")}
+                      >
+                        Game Over
+                      </Button>
+                    </Col>
+                  </Row>
+                </div>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
